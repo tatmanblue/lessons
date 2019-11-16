6aa6d160b91ffaa23e3218aef91a14fecbf84c7e
tatmanblue <Matt.Raffel@gmail.com>
2019-11-16 09:04:31 -0700

    implements step 1
    Signed-off-by: tatmanblue <Matt.Raffel@gmail.com>

---
+++ cpp/multi-dimenion-array/README.md
@@ -0,0 +1,9 @@
+# Two dimension arrays
+The purpose of this lesson is to learn about 2 dimension arrays
+
+
+# Requirements
+1. Ask the user how many elements in an array to use
+2. Randomly assign values into the arrays
+3. Add the values the arrays and output result
+4. Multiply the values the arrays and output result
--- cpp/multi-dimenion-array/step1/Step1.cpp
+++ cpp/multi-dimenion-array/step1/Step1.cpp
@@ -1,20 +1,19 @@
-// Step1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+/*
+Creates two square Matrices
+then calls functions to add, subtract, then multiply them
+*/
+#include<iostream>
+using namespace std;
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "1. Ask the user how many elements in an array to use" << endl;
+	cout << "2. Randomly assign values into the arrays" << endl;
+	cout << "3. Add the values the arrays and output result" << endl;
+	cout << "4. Multiply the values the arrays and output result" << endl;
+	cout << "5. Ask the user if they want to try again" << endl;
+
+	cout << "app is done";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
